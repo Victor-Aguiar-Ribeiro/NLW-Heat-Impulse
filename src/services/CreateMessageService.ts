16206ba4736177805c261d304a113d2f1aadd1d7
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-10-20 16:25:32 -0300

    Adicionando o arquivo CreateMessageService.ts na pasta services

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,15 @@
+﻿/* Serviço de mensagem
+*
+* recebe uma mensagem : string e o usuário que enviou : string 
+*
+*/
+
+class CreateMessageService {
+
+   async execute( text: string, user_id: string){
+
+   };
+
+};
+
+export { CreateMessageService };

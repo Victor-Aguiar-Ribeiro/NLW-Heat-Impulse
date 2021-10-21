38b5395621ad1ee24ab7c18fbd97852489449170
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-10-21 03:54:33 -0300

    Adicionado o arquivo server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,39 @@
 ﻿import "dotenv/config";
 import express from "express";
-
 import { router } from "./routes"
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
-app.use(express.json());
+app.use (cors());
 
-app.use(router);
+const serverHttp = http.createServer(app);
 
-app.get("/github", (request, response) => {
+const io = new Server ( serverHttp , {
+   cors: {
+      origin: "*",
+   },
+});
+
+io.on ( "connection", socket => {
+   console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use (express.json());
+
+app.use (router);
+
+app.get ( "/github" , ( request , response ) => {
    response.redirect(
       `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
    );
 });
 
-app.get("/signin/callback" , (request, response) =>{
+app.get ("/signin/callback" , ( request , response ) =>{
    const {code} = request.query;
 
    return response.json(code);
 });
 
-app.listen(4000 , () => {console.log(`🚀 Server is running on port: http://localhost:4000/`)});
+export { serverHttp , io };
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+﻿import { serverHttp } from "./app";
+
+
+serverHttp.listen ( 4000 , () => {console.log( `🚀 Server is running on port: http://localhost:4000/` )});

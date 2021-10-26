175711551522c0b12242dab62741d6d37b3c7933
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-10-26 19:30:55 -0300

    Aplicação web praticamente finalizada - (Ainda é necessário verificar o funcionamento da lista de mensagens, não está ocorrendo o intervalo de tempo entre novas mensagens.

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@
 import styles from './styles.module.scss';
 import logoImage from '../../assets/logo.svg';
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 type Message = {
   id: string;
@@ -11,14 +12,36 @@ type Message = {
     avatar_url: string;
   }
 }
+// Criando lista de mensagens;
+const messagesQueue: Message[] = [];
 
+const socket = io( 'http://localhost:4000' );
+
+// Adiciona cada nova mensagem na fila
+socket.on( 'new_message', ( newMessage: Message ) => {
+  messagesQueue.push( newMessage );
+})
 
 export function MessageList() {
-  const [messages, setMessages] = useState <Message[]> ([]);
+  const [ messages, setMessages ] = useState< Message[] >([]);
+
+  // Seta as mensagens da fila para irem aparecendo a cada 3 segundos
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if( messagesQueue.length > 0){
+        setMessages(prevState =>[
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter( Boolean ))
+        messagesQueue.shift();
+      }
+    }, 10000);
+  }, [] )
 
   useEffect(() => { (
-    api.get<Message[]>('messages/last3').then((response) => {
-      setMessages(response.data);
+    api.get<Message[]>( 'messages/last3' ).then(( response ) => {
+      setMessages( response.data );
     })
   ) , [] })
 
@@ -28,7 +51,7 @@ export function MessageList() {
       <ul className = { styles.messageList }>
         { messages.map ( message => {
           return(
-            <li key={ message.id}className = { styles.message }>
+            <li key={ `key_${message.id}` } className = { styles.message }>
               <p className = { styles.messageContent }> { message.text } </p>
               <div className = { styles.messageUser }>
                 <div className = { styles.userImage }>
@@ -39,7 +62,6 @@ export function MessageList() {
             </li>
           );
         })}
-
       </ul>
     </div>
   );

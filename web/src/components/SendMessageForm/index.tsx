317087684851f8cc1a21c366a272d769f6581cbd
Victor-Aguiar-Ribeiro <victor_a.ribeiro@outlook.com>
2021-10-26 12:35:45 -0300

    Finalizada a estilização do componente SendMessageForm

--- web/src/components/SendMessageForm/index.tsx
+++ web/src/components/SendMessageForm/index.tsx
@@ -1,7 +1,41 @@
-﻿export function SendMessageForm(){
+﻿import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+import styles from './styles.module.scss';
+import { VscGithubInverted, VscSignOut } from 'react-icons/vsc'
 
+export function SendMessageForm(){
+  const { user } = useContext(AuthContext);
 
   return (
-    <h1>SendMessageForm</h1>
+    <div className={ styles.sendMessageFormWrapper }>
+      <button className={ styles.signOutButton }>
+        <VscSignOut size='32'/>
+      </button>
+
+      <header className={ styles.userInformation }>
+        <div className={ styles.userImage }>
+          <img src= { user?.avatar_url } alt={ user?.name } />
+        </div>
+
+        <strong className={ styles.userName }>{ user?.name }</strong>
+
+        <span className={ styles.userGithub }>
+          <VscGithubInverted size='16' />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={ styles.sendMessageForm }>
+        <label htmlFor="message">Mensagem</label>
+
+        <textarea
+          name="message" 
+          id="message" 
+          placeholder="Qual sua expectativa para o evento?"
+        />
+
+        <button type="submit">Enviar Mensagem</button>
+      </form>
+    </div>
   )
 }
